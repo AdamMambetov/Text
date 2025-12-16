96e090558afdcc31f12e8f1ea97d48c4bbe9c8b4
AdamMambetov <adammambetov.342@gmail.com>
2025-12-16 06:01:11 +0300

    vault backup: 2025-12-16 06:00:53

--- Note/scripts/termux-scripts/git-sync-root.sh
+++ Note/scripts/termux-scripts/git-sync-root.sh
@@ -3,6 +3,6 @@ git status
 
 COMMIT_MSG="vault backup: $(date +"%F %T")"
 git add . && git commit -m "$COMMIT_MSG"
-git submodule foreach "git add . && git commit -m \"$COMMIT_MSG\" || true"
+git submodule foreach "git checkout master && git add . && git commit -m \"$COMMIT_MSG\" || true"
 git add . && git commit -m "Update submodules: $COMMIT_MSG"
-git push && git submodule foreach "git push || true" 
+git push && git submodule foreach "git push origin master || true" 
