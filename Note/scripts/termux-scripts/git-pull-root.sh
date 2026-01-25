2a79fe9bd107a66a9dd652110ee6accfafedb3a8
AdamMambetov <adammambetov.342@gmail.com>
2026-01-25 03:11:30 +0300

    vault backup: 2026-01-25 03:11:30

--- Note/scripts/termux-scripts/git-pull-root.sh
+++ Note/scripts/termux-scripts/git-pull-root.sh
@@ -1,5 +1,3 @@
 cd /storage/emulated/0/_Adam/Text/
-git checkout master
 git pull
-git pull --recurse-submodules
-git submodule foreach "git checkout master && git pull"
+git submodule foreach "git pull"
--- Note/scripts/termux-scripts/git-sync-root.sh
+++ Note/scripts/termux-scripts/git-sync-root.sh
@@ -2,7 +2,6 @@ cd /storage/emulated/0/_Adam/Text/
 git status
 
 COMMIT_MSG="vault backup: $(date +"%F %T")"
+git submodule foreach "git add . && git commit -m \"$COMMIT_MSG\""
 git add . && git commit -m "$COMMIT_MSG"
-git submodule foreach "git checkout master && git add . && git commit -m \"$COMMIT_MSG\" || true"
-git add . && git commit -m "Update submodules: $COMMIT_MSG"
-git push && git submodule foreach "git push origin master || true" 
+git push && git submodule foreach "git push origin master || true"
